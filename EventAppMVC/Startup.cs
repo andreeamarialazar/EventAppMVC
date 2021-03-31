2ea19ecb50e7c92b0ffcc8dbd6724e14a99f6370
ANDREEA-MARIA LAZAR <andreea.maria.lazar@stud.ubbcluj.ro>
2021-03-31 21:57:00 +0300

    first

---
+++ EventAppMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventAppMVC.Startup))]
+namespace EventAppMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
